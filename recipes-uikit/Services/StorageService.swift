9d52153829bf89e6734223c0066dcb044c78ea45
Sergey Bendak <sleepoke@gmail.com>
2024-09-03 16:50:07 +0200

    extend StorageService with history methods

--- recipes-uikit/Services/StorageService.swift
+++ recipes-uikit/Services/StorageService.swift
@@ -10,12 +10,15 @@ import Foundation
 protocol StorageServiceProtocol {
     func loadLastViewedRecipe() -> Recipe?
     func saveLastRecipe(_ recipe: Recipe)
+    func loadRecipeHistory() -> [Recipe]
+    func saveRecipeToHistory(_ recipe: Recipe)
 }
 
 final class StorageService: StorageServiceProtocol {
     
     private let userDefaults = UserDefaults.standard
     private let recipeKey = "lastRecipe"
+    private let recipeHistoryKey = "recipeHistory"
     
     func loadLastViewedRecipe() -> Recipe? {
         if let data = userDefaults.data(forKey: recipeKey),
@@ -29,6 +32,22 @@ final class StorageService: StorageServiceProtocol {
     func saveLastRecipe(_ recipe: Recipe) {
         if let data = try? JSONEncoder().encode(recipe) {
             userDefaults.set(data, forKey: recipeKey)
+        }
+    }
+    
+    func loadRecipeHistory() -> [Recipe] {
+        if let data = userDefaults.data(forKey: recipeHistoryKey),
+           let savedHistory = try? JSONDecoder().decode([Recipe].self, from: data) {
+            return savedHistory
+        }
+        return []
+    }
+    
+    func saveRecipeToHistory(_ recipe: Recipe) {
+        var history = loadRecipeHistory()
+        history.append(recipe)
+        if let data = try? JSONEncoder().encode(history) {
+            userDefaults.set(data, forKey: recipeHistoryKey)
         }
     }
 }
