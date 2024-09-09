02ce3da1f1d2d9e6a68065d6918f414a64337a22
Sergey Bendak <sleepoke@gmail.com>
2024-09-09 11:25:13 +0200

    add tests target

---
+++ recipes-uikit-tests/recipes_uikit_tests.swift
@@ -0,0 +1,37 @@
+//
+//  recipes_uikit_tests.swift
+//  recipes-uikit-tests
+//
+//  Created by Sergey Bendak on 9.09.2024.
+//
+
+import XCTest
+
+@testable import recipes_uikit
+
+final class recipes_uikit_tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
